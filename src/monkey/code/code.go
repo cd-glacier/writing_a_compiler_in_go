7bd261ce0673236ceab38c7c8a8c2c72943ce9cc
g-hyoga <hyoga0216@gmail.com>
2019-04-22 08:44:44 +0900

    Implement Add, Sub, Mul, Div

--- src/monkey/code/code.go
+++ src/monkey/code/code.go
@@ -12,8 +12,11 @@ type Opcode byte
 
 const (
 	OpConstant Opcode = iota
-	OpAdd
 	OpPop
+	OpAdd
+	OpSub
+	OpMul
+	OpDiv
 )
 
 type Definition struct {
@@ -23,8 +26,11 @@ type Definition struct {
 
 var definitions = map[Opcode]*Definition{
 	OpConstant: {"OpConstant", []int{2}},
-	OpAdd:      {"OpAdd", []int{}},
 	OpPop:      {"OpPop", []int{}},
+	OpAdd:      {"OpAdd", []int{}},
+	OpSub:      {"OpSub", []int{}},
+	OpMul:      {"OpMul", []int{}},
+	OpDiv:      {"OpDiv", []int{}},
 }
 
 func Lookup(op byte) (*Definition, error) {
