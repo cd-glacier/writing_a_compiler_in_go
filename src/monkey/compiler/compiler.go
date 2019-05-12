361cecf9d783c95cb5a451a80a4abe2832cae8b3
g-hyoga <hyoga0216@gmail.com>
2019-05-12 18:57:35 +0900

    Add string

--- src/monkey/compiler/compiler.go
+++ src/monkey/compiler/compiler.go
@@ -137,6 +137,10 @@ func (c *Compiler) Compile(node ast.Node) error {
 	case *ast.Identifier:
 		return c.compileIdentifier(node)
 
+	case *ast.StringLiteral:
+		str := &object.String{Value: node.Value}
+		c.emit(code.OpConstant, c.addConstant(str))
+
 	}
 
 	return nil
--- src/monkey/vm/vm.go
+++ src/monkey/vm/vm.go
@@ -242,11 +242,16 @@ func (vm *VM) executeBinaryOperation(op code.Opcode) error {
 	leftType := left.Type()
 	rightType := right.Type()
 
-	if leftType == object.INTEGER_OBJ && rightType == object.INTEGER_OBJ {
+	switch {
+	case leftType == object.INTEGER_OBJ && rightType == object.INTEGER_OBJ:
 		return vm.execueteBinaryIntegerOpration(op, left, right)
-	}
 
-	return fmt.Errorf("unsupported types for binary operation: %s %s", leftType, rightType)
+	case leftType == object.STRING_OBJ && rightType == object.STRING_OBJ:
+		return vm.execueteBinaryStringOpration(op, left, right)
+
+	default:
+		return fmt.Errorf("unsupported type for binary operation: %s %s", leftType, rightType)
+	}
 }
 
 func (vm *VM) execueteBinaryIntegerOpration(op code.Opcode, left, right object.Object) error {
@@ -269,6 +274,17 @@ func (vm *VM) execueteBinaryIntegerOpration(op code.Opcode, left, right object.O
 	}
 
 	return vm.push(&object.Integer{Value: result})
+}
+
+func (vm *VM) execueteBinaryStringOpration(op code.Opcode, left, right object.Object) error {
+	if op != code.OpAdd {
+		return fmt.Errorf("unknown string operator: %d", op)
+	}
+
+	leftValue := left.(*object.String).Value
+	rightValue := right.(*object.String).Value
+
+	return vm.push(&object.String{Value: leftValue + rightValue})
 }
 
 func isTruthy(obj object.Object) bool {
